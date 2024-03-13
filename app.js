9169152839951ecda9b29ec7675c11e37f8ed7d3
SHABI9999 <you@example.com>
2024-03-14 04:01:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var personName = "MUSTAFA";
+console.log("helow ".concat(personName, " would you like to learn some Typescript today?"));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personName: string="MUSTAFA";
+console.log(`helow ${personName} would you like to learn some Typescript today?`);
